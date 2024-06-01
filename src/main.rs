2418e48e7cda8a02c98b1f2c907255f01b47d629
Jake <dominik8655@gmail.com>
2024-06-01 18:53:36 +0200

    feat: handle missing commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
-use std::io::{self, Write};
+use colored::Colorize;
+use std::{
+    io::{self, Write},
+    str::FromStr,
+};
+
+enum Command {}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(_: &str) -> Result<Self, Self::Err> {
+        // match s {
+        /* _ =>  */
+        Err(()) /* , */
+        // }
+    }
+}
 
 fn main() {
+    print_prompt();
+
+    let input = read_line();
+    let trimmed = input.trim();
+
+    let command = Command::from_str(trimmed);
+
+    match command {
+        Ok(_cmd) => {}
+        Err(_) => println!("{}: {} not found", trimmed, "command".red()),
+    }
+}
+
+fn print_prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
+}
 
-    // Wait for user input
-    let stdin = io::stdin();
+fn read_line() -> String {
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    input
 }
